e76f537b9fd5a1ad605b003082cff8a060dd89ea
Kevin Daughtridge <kevin@kdau.com>
2013-02-24 21:11:13 -0800

    ChessBoard: new class. ChessGame: add piece and move selection; move persistence to ChessBoard. ChessSquare: new script. ChessPiece: new script. LinkIter, ScriptParamsIter: import from pictura.

--- custom.cpp
+++ custom.cpp
@@ -38,6 +38,129 @@
 
 
 
+/* LinkIter */
+
+LinkIter::LinkIter (object source, object dest, const char* flavor)
+{
+	SService<ILinkSrv> pLS (g_pScriptManager);
+	SService<ILinkToolsSrv> pLTS (g_pScriptManager);
+	pLS->GetAll (m_links, flavor ? pLTS->LinkKindNamed (flavor) : 0,
+		source, dest);
+}
+
+LinkIter::~LinkIter () noexcept
+{}
+
+LinkIter::operator bool () const
+{
+	return m_links.AnyLinksLeft ();
+}
+
+LinkIter&
+LinkIter::operator++ ()
+{
+	m_links.NextLink ();
+	AdvanceToMatch ();
+	return *this;
+}
+
+LinkIter::operator link () const
+{
+	return m_links.AnyLinksLeft () ? m_links.Link () : link ();
+}
+
+object
+LinkIter::Source () const
+{
+	return m_links.AnyLinksLeft () ? m_links.Get ().source : object ();
+}
+
+object
+LinkIter::Destination () const
+{
+	return m_links.AnyLinksLeft () ? m_links.Get ().dest : object ();
+}
+
+const void*
+LinkIter::GetData () const
+{
+	return m_links.AnyLinksLeft () ? m_links.Data () : NULL;
+}
+
+void
+LinkIter::GetDataField (const char* field, cMultiParm& value) const
+{
+	if (!field)
+		throw std::invalid_argument ("invalid link data field");
+	SService<ILinkToolsSrv> pLTS (g_pScriptManager);
+	pLTS->LinkGetData (value, m_links.Link (), field);
+}
+
+void
+LinkIter::AdvanceToMatch ()
+{
+	while (m_links.AnyLinksLeft () && !Matches ())
+		m_links.NextLink ();
+}
+
+
+
+/* ScriptParamsIter */
+
+ScriptParamsIter::ScriptParamsIter (object source, const char* data,
+		object destination)
+	: LinkIter (source, destination, "ScriptParams"),
+	  m_data (data), m_only ()
+{
+	if (!source)
+		throw std::invalid_argument ("invalid source object");
+	if (data && !strcmp (data, "Self"))
+		m_only = source;
+	else if (data && !strcmp (data, "Player"))
+		m_only = StrToObject ("Player");
+	AdvanceToMatch ();
+}
+
+ScriptParamsIter::~ScriptParamsIter () noexcept
+{}
+
+ScriptParamsIter::operator bool () const
+{
+	return m_only ? true : LinkIter::operator bool ();
+}
+
+ScriptParamsIter&
+ScriptParamsIter::operator++ ()
+{
+	if (m_only)
+		m_only = 0;
+	else
+		LinkIter::operator++ ();
+	return *this;
+}
+
+ScriptParamsIter::operator object () const
+{
+	if (m_only)
+		return m_only;
+	else
+		return Destination ();
+}
+
+bool
+ScriptParamsIter::Matches ()
+{
+	if (!LinkIter::operator bool ())
+		return false;
+	else if (m_data)
+		return !strnicmp (m_data, (const char*) GetData (),
+			m_data.GetLength () + 1);
+	else
+		return true;
+}
+
+
+
 /* ChessEngine */
 
 #define ENGINE_BUFSIZE 4096
@@ -204,15 +327,293 @@ ChessEngine::EngineThread (void* _pipefd)
 
 
 
-/* ChessGame */
+/* ChessBoard::Square */
 
-#define STARTING_POSITION \
-	"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+ChessBoard::Square::Square (File _file, Rank _rank)
+	: file (_file), rank (_rank)
+{}
+
+ChessBoard::Square::operator bool () const
+{
+	return file >= FILE_a && file <= FILE_h &&
+		rank >= RANK_1 && rank <= RANK_8;
+}
+
+ChessBoard::Square
+ChessBoard::Square::Offset (Square by) const
+{
+	Square result;
+	result.file = File (file + by.file);
+	result.rank = Rank (rank + by.rank);
+	return result ? result : Square ();
+}
+
+
+
+/* ChessBoard::Piece */
+
+#define BLACK_DELTA ('a' - 'A')
+
+ChessBoard::Piece::Piece (Camp _camp, PieceType _type)
+	: camp (_camp), type (_type)
+{}
+
+ChessBoard::Piece::Piece (char code)
+	: camp (CAMP_NONE), type (PIECE_NONE)
+{
+	char code2 = code - BLACK_DELTA;
+	if (code == PIECE_KING || code == PIECE_QUEEN ||
+		code == PIECE_ROOK || code == PIECE_BISHOP ||
+		code == PIECE_KNIGHT || code == PIECE_PAWN)
+	{
+		camp = CAMP_WHITE;
+		type = (PieceType) code;
+	}
+	else if (code2 == PIECE_KING || code2 == PIECE_QUEEN ||
+		code2 == PIECE_ROOK || code2 == PIECE_BISHOP ||
+		code2 == PIECE_KNIGHT || code2 == PIECE_PAWN)
+	{
+		camp = CAMP_BLACK;
+		type = (PieceType) code2;
+	}
+}
+
+ChessBoard::Piece::operator char () const
+{
+	if (camp == CAMP_WHITE)
+		return type;
+	else if (camp == CAMP_BLACK)
+		return type + BLACK_DELTA;
+	else
+		return PIECE_NONE;
+}
+
+
+
+/* ChessBoard */
+
+const char ChessBoard::INITIAL_PLACEMENT[65] =
+	"RNBQKBNRPPPPPPPP                                pppppppprnbqkbnr";
+
+ChessBoard::ChessBoard ()
+	: active_camp (CAMP_WHITE),
+	  castling_white (CASTLE_KINGSIDE | CASTLE_QUEENSIDE),
+	  castling_black (CASTLE_KINGSIDE | CASTLE_QUEENSIDE),
+	  en_passant_square (FILE_NONE, RANK_NONE),
+	  halfmove_clock (0),
+	  fullmove_number (1)
+{
+	strncpy (piece_placement, INITIAL_PLACEMENT, 65);
+}
+
+ChessBoard::Piece
+ChessBoard::operator[] (const Square& square) const
+{
+	if (!square) return '\0';
+	return Piece (piece_placement
+		[(square.rank - RANK_1) * 8 + (square.file - FILE_a)]);
+}
+
+void
+ChessBoard::EncodeFEN (char* buffer)
+{
+	if (!buffer) return;
+	char* ptr;
+
+	char board_fen[FEN_BUFSIZE]; ptr = board_fen;
+	//FIXME Read off board into board_fen in FEN format.
+
+	char castling[5]; ptr = castling;
+	if (castling_white & CASTLE_KINGSIDE) *ptr++ = 'K';
+	if (castling_white & CASTLE_QUEENSIDE) *ptr++ = 'Q';
+	if (castling_black & CASTLE_KINGSIDE) *ptr++ = 'k';
+	if (castling_black & CASTLE_QUEENSIDE) *ptr++ = 'q';
+	if (ptr == castling) *ptr++ = '-';
+	*ptr = '\0';
+
+	char en_passant[3] = "-\0";
+	if (en_passant_square)
+	{
+		en_passant[0] = en_passant_square.file;
+		en_passant[1] = en_passant_square.rank;
+	}
+
+	snprintf (buffer, FEN_BUFSIZE, "%s %c %s %s %u %u", board_fen,
+		(active_camp == CAMP_WHITE) ? 'w' : 'b',
+		castling, en_passant, halfmove_clock, fullmove_number);
+}
+
+#define load_script_int(type, name) \
+	{ script_int _##name ("ChessGame", #name, store); \
+	if (_##name.Valid ()) name = (type) (int) _##name; }
+
+void
+ChessBoard::Load (object store)
+{
+	script_str _piece_placement ("ChessGame", "piece_placement", store);
+	if (_piece_placement.Valid ())
+		strncpy (piece_placement, _piece_placement, 65);
+
+	load_script_int (Camp, active_camp);
+	load_script_int (int, castling_white);
+	load_script_int (int, castling_black);
+
+	script_str _eps ("ChessGame", "en_passant_square", store);
+	if (!_eps.Valid ())
+		{}
+	else if (!strcmp (_eps, "-"))
+		en_passant_square = Square ();
+	else if (strlen (_eps) == 2)
+		en_passant_square = Square ((File) _eps[0], (Rank) _eps[1]);
+
+	load_script_int (uint, halfmove_clock);
+	load_script_int (uint, fullmove_number);
+}
+
+#undef load_script_var
+#define save_script_var(type, name) \
+	{ script_##type ("ChessGame", #name, store) = name; }
+
+void
+ChessBoard::Save (object store)
+{
+	save_script_var (str, piece_placement);
+	save_script_var (int, active_camp);
+	save_script_var (int, castling_white);
+	save_script_var (int, castling_black);
+
+	char eps[3] = "XX";
+	eps[0] = en_passant_square.file;
+	eps[1] = en_passant_square.rank;
+	script_str ("ChessGame", "en_passant_square", store) = eps;
+
+	save_script_var (int, halfmove_clock);
+	save_script_var (int, fullmove_number);
+}
+
+#undef save_script_var
+
+void
+ChessBoard::EnumerateMoves (MoveSet& set)
+{
+	set.clear ();
+	for (char rank = RANK_1; rank <= RANK_8; ++rank)
+	for (char file = FILE_a; file <= FILE_h; ++file)
+	{
+		Square origin ((File) file, (Rank) rank);
+		Piece piece = (*this)[origin];
+
+		switch (piece.type)
+		{
+		case PIECE_ROOK:
+		case PIECE_BISHOP:
+		case PIECE_QUEEN:
+			if (piece.type != PIECE_ROOK)
+			{
+				TryMovePath (set, piece, origin,  1,  1);
+				TryMovePath (set, piece, origin, -1,  1);
+				TryMovePath (set, piece, origin,  1, -1);
+				TryMovePath (set, piece, origin, -1, -1);
+			}
+			if (piece.type != PIECE_BISHOP)
+			{
+				TryMovePath (set, piece, origin,  0,  1);
+				TryMovePath (set, piece, origin,  0, -1);
+				TryMovePath (set, piece, origin,  1,  0);
+				TryMovePath (set, piece, origin, -1,  0);
+			}
+			break;
+		case PIECE_KNIGHT:
+			TryMovePoint (set, piece, origin,  1,  2);
+			TryMovePoint (set, piece, origin,  2,  1);
+			TryMovePoint (set, piece, origin, -1,  2);
+			TryMovePoint (set, piece, origin, -2,  1);
+			TryMovePoint (set, piece, origin,  1, -2);
+			TryMovePoint (set, piece, origin,  2, -1);
+			TryMovePoint (set, piece, origin, -1, -2);
+			TryMovePoint (set, piece, origin, -2, -1);
+			break;
+		case PIECE_KING:
+			TryMovePoint (set, piece, origin,  1,  1);
+			TryMovePoint (set, piece, origin,  1,  0);
+			TryMovePoint (set, piece, origin,  1, -1);
+			TryMovePoint (set, piece, origin,  0, -1);
+			TryMovePoint (set, piece, origin, -1, -1);
+			TryMovePoint (set, piece, origin, -1,  0);
+			TryMovePoint (set, piece, origin, -1,  1);
+			TryMovePoint (set, piece, origin,  0,  1);
+			//FIXME Consider castling.
+			break;
+		case PIECE_PAWN:
+		    {
+			char dirn = (piece.camp == CAMP_WHITE) ? 1 : -1;
+			char orig_rank = (piece.camp == CAMP_WHITE) ? RANK_2 : RANK_7;
+			bool one_okay =
+				TryMovePoint (set, piece, origin, 0, dirn, false);
+			if (one_okay && (rank == orig_rank))
+				TryMovePoint (set, piece, origin, 0, 2 * dirn, false);
+			TryMovePoint (set, piece, origin, -1, dirn, true, false);
+			TryMovePoint (set, piece, origin,  1, dirn, true, false);
+			//FIXME Consider en passant capture.
+			break;
+		    }
+		default:
+			break;
+		}
+	}
+}
+
+void
+ChessBoard::TryMovePath (MoveSet& set, Piece piece, Square origin,
+	char file_dirn, char rank_dirn)
+{
+	char file_delta = 0, rank_delta = 0;
+	while (true)
+	{
+		file_delta += file_dirn;
+		rank_delta += rank_dirn;
+		if (TryMovePoint (set, piece, origin, file_delta, rank_delta,
+				true, false)) // if_hostile, !if_empty
+			return; // piece capture required, so can't go farther
+		if (!TryMovePoint (set, piece, origin, file_delta, rank_delta,
+				false, true)) // !if_hostile, if_empty
+			return; // something else in the way
+	}
+}
+
+bool
+ChessBoard::TryMovePoint (MoveSet& set, Piece piece, Square origin,
+	char file_vect, char rank_vect, bool if_hostile, bool if_empty)
+{
+	Square offset ((File) file_vect, (Rank) rank_vect);
+	Square destination = origin.Offset (offset);
+	if (!destination) return false; // invalid position
+
+	Piece occupant = (*this)[destination];
+	if (occupant)
+	{
+		if (occupant.camp == piece.camp)
+			return false; // friendly occupant
+		else if (!if_hostile)
+			return false;
+	}
+	else if (!if_empty)
+		return false;
+
+	//FIXME Filter otherwise illegal moves.
+
+	Move move = { origin, destination };
+	set.push_back (move);
+	return true;
+}
+
+
+
+/* ChessGame */
 
 cScr_ChessGame::cScr_ChessGame (const char* pszName, int iHostObjId)
 	: cBaseScript (pszName, iHostObjId),
-	  engine (NULL),
-	  SCRIPT_VAROBJ (ChessGame, position, iHostObjId)
+	  engine (NULL)
 {}
 
 long
@@ -235,10 +636,18 @@ cScr_ChessGame::OnBeginScript (sScrMsg*, cMultiParm&)
 
 	engine->WaitUntilReady ();
 
-	if (position.Valid ()) // not a new game (which is started in OnSim)
-	{
+	if (script_var ("ChessGame", "piece_placement", ObjId ()).Valid ())
+	{ // game in progress
+		board.Load (ObjId ());
+		char position[FEN_BUFSIZE];
+		board.EncodeFEN (position);
 		engine->StartGame (position);
-		//FIXME Anything else? What if it's the computer's move?
+		//FIXME Anything else now? What if it's the computer's move? Is that even a valid state to persist?
+	}
+	else // new game
+	{
+		board.Save (ObjId ());
+		engine->StartGame (NULL);
 	}
 
 	return 0;
@@ -260,12 +669,192 @@ cScr_ChessGame::OnSim (sSimMsg* pMsg, cMultiParm&)
 {
 	if (pMsg->fStarting && engine)
 	{
-		engine->StartGame (NULL);
-		position = STARTING_POSITION; //FIXME Read off the objects instead?
+		//FIXME What, if any, of this should be in BeginScript or v-v?
+		AnalyzeBoard ();
 		//FIXME Begin the game.
 	}
 	return 0;
 }
 
-//FIXME Crashing on DromEd exit.
+long
+cScr_ChessGame::OnMessage (sScrMsg* pMsg, cMultiParm& mpReply)
+{
+	if (!strcmp (pMsg->message, "SelectPiece"))
+		SelectPiece (pMsg->from);
+
+	else if (!strcmp (pMsg->message, "MakeMove"))
+		MakeMove (pMsg->from);
+
+	return cBaseScript::OnMessage (pMsg, mpReply);
+}
+
+object
+cScr_ChessGame::GetSquare (ChessBoard::Square _square)
+{
+	char square_name[9] = "SquareXX";
+	square_name[6] = _square.file;
+	square_name[7] = _square.rank;
+	return StrToObject (square_name);
+}
+
+object
+cScr_ChessGame::GetPieceAt (ChessBoard::Square _square)
+{
+	object square = GetSquare (_square);
+	return square
+		? LinkIter (square, 0, "Population").Destination ()
+		: object ();
+}
+
+void
+cScr_ChessGame::AnalyzeBoard ()
+{
+	//FIXME When/where are the persistent variables updated?
+
+	// erase old possible-move links
+	SInterface<ITraitManager> pTM (g_pScriptManager);
+	object archetype = StrToObject ("ChessSquare");
+	for (LinkIter old_move (0, 0, "Route"); old_move; ++old_move)
+		if (pTM->ObjHasDonor (old_move.Destination (), archetype))
+			DestroyLink (old_move);
+
+	// create new possible-move links
+	ChessBoard::MoveSet moves;
+	board.EnumerateMoves (moves);
+	for (ChessBoard::MoveSet::iterator move = moves.begin ();
+		move != moves.end (); ++move)
+	{
+		object piece = GetPieceAt (move->from),
+			destination = GetSquare (move->to);
+		if (piece && destination)
+			CreateLink ("Route", piece, destination);
+	}
+
+	//FIXME Check for checks/checkmates/stalemates/etc. and react accordingly.
+}
+
+void
+cScr_ChessGame::SelectPiece (object piece)
+{
+	//FIXME Confirm that game state is okay for piece selection. Or block frob entirely when it's not...
+	DeselectPieces ();
+	CreateLink ("ScriptParams", ObjId (), piece, "SelectedPiece");
+	SimpleSend (ObjId (), piece, "Select");
+}
+
+void
+cScr_ChessGame::DeselectPieces ()
+{
+	SService<IPropertySrv> pPS (g_pScriptManager);
+
+	for (ScriptParamsIter old (ObjId (), "SelectedPiece"); old; ++old)
+	{
+		SimpleSend (ObjId (), old.Destination (), "Deselect");
+		DestroyLink (old);
+	}
+}
+
+void
+cScr_ChessGame::MakeMove (object square)
+{
+	//FIXME Confirm that game state is okay for movement. Or block frob entirely when it's not...
+
+	object piece = GetOneLinkByDataDest
+		("ScriptParams", ObjId (), "SelectedPiece", -1);
+	DeselectPieces ();
+	if (!piece || !square) return;
+
+	//FIXME Perform the move of @piece to @square. Proceed accordingly from there.
+}
+
+
+
+/* ChessSquare */
+
+cScr_ChessSquare::cScr_ChessSquare (const char* pszName, int iHostObjId)
+	: cBaseScript (pszName, iHostObjId)
+{}
+
+long
+cScr_ChessSquare::OnMessage (sScrMsg* pMsg, cMultiParm& mpReply)
+{
+	if (!strcmp (pMsg->message, "EnableMove"))
+	{
+		AddSingleMetaProperty ("M-PossibleMove", ObjId ());
+
+		SService<IPropertySrv> pPS (g_pScriptManager);
+		pPS->Add (ObjId (), "SelfLit"); // dynamic light
+	}
+
+	else if (!strcmp (pMsg->message, "DisableMove"))
+	{
+		RemoveSingleMetaProperty ("M-PossibleMove", ObjId ());
+
+		SService<IPropertySrv> pPS (g_pScriptManager);
+		pPS->Remove (ObjId (), "SelfLit"); // dynamic light
+	}
+
+	return cBaseScript::OnMessage (pMsg, mpReply);
+}
+
+long
+cScr_ChessSquare::OnFrobWorldEnd (sFrobMsg*, cMultiParm&)
+{
+	SimpleSend (ObjId (), StrToObject ("TheGame"), "MakeMove");
+	return 0;
+}
+
+
+
+/* ChessPiece */
+
+cScr_ChessPiece::cScr_ChessPiece (const char* pszName, int iHostObjId)
+	: cBaseScript (pszName, iHostObjId),
+	  cBaseAIScript (pszName, iHostObjId)
+{}
+
+long
+cScr_ChessPiece::OnMessage (sScrMsg* pMsg, cMultiParm& mpReply)
+{
+	if (!strcmp (pMsg->message, "Select"))
+		Select ();
+
+	else if (!strcmp (pMsg->message, "Deselect"))
+		Deselect ();
+
+	return cBaseScript::OnMessage (pMsg, mpReply);
+}
+
+long
+cScr_ChessPiece::OnFrobWorldEnd (sFrobMsg*, cMultiParm&)
+{
+	SimpleSend (ObjId (), StrToObject ("TheGame"), "SelectPiece");
+	return 0;
+}
+
+void
+cScr_ChessPiece::Select ()
+{
+	AddSingleMetaProperty ("M-SelectedPiece", ObjId ());
+
+	SService<IPropertySrv> pPS (g_pScriptManager);
+	pPS->Add (ObjId (), "SelfLit"); // dynamic light
+
+	for (LinkIter move (ObjId (), 0, "Route"); move; ++move)
+		SimpleSend (ObjId (), move.Destination (), "EnableMove");
+
+	PlaySchemaAmbient (ObjId (), StrToObject ("bow_begin"));
+}
+
+void
+cScr_ChessPiece::Deselect ()
+{
+	RemoveSingleMetaProperty ("M-SelectedPiece", ObjId ());
+
+	SService<IPropertySrv> pPS (g_pScriptManager);
+	pPS->Remove (ObjId (), "SelfLit"); // dynamic light
+
+	for (LinkIter move (ObjId (), 0, "Route"); move; ++move)
+		SimpleSend (ObjId (), move.Destination (), "DisableMove");
+}
 
--- custom.h
+++ custom.h
@@ -29,11 +29,80 @@
 
 #include <windows.h>
 #undef GetClassName // ugh
+#include <vector>
 
 #include "BaseScript.h"
 #include "scriptvars.h"
 
 #endif // SCR_GENSCRIPTS
+
+
+
+/**
+ * Iterator: LinkIter
+ *
+ * Iterates through links that share a source, destination, and/or flavor.
+ * Subclasses can further limit the set (for example, based on data) by
+ * overriding the Matches function (and calling AdvanceToMatch in their ctors).
+ */
+#if !SCR_GENSCRIPTS
+class LinkIter
+{
+public:
+	LinkIter (object source, object dest, const char* flavor);
+	virtual ~LinkIter () noexcept;
+
+	virtual operator bool () const;
+	virtual LinkIter& operator++ ();
+
+	operator link () const;
+	object Source () const;
+	object Destination () const;
+
+	const void* GetData () const;
+	void GetDataField (const char* field, cMultiParm& value) const;
+
+protected:
+	void AdvanceToMatch ();
+	virtual bool Matches () { return true; }
+
+private:
+	linkset m_links;
+};
+#endif // SCR_GENSCRIPTS
+
+
+
+/**
+ * Iterator: ScriptParamsIter
+ *
+ * Iterates through ScriptParams links from a source object that share a certain
+ * link data string. If the string is "Self" or "Player", iterates over only the
+ * source object or the object named Player, respectively.
+ */
+#if !SCR_GENSCRIPTS
+class ScriptParamsIter : public LinkIter
+{
+public:
+	ScriptParamsIter (object source, const char* data,
+		object destination = object ());
+	virtual ~ScriptParamsIter () noexcept;
+
+	virtual operator bool () const;
+	virtual ScriptParamsIter& operator++ ();
+
+	operator object () const;
+
+protected:
+	virtual bool Matches ();
+
+private:
+	cAnsiStr m_data;
+	object m_only;
+};
+#endif // SCR_GENSCRIPTS
+
+
 
 #if !SCR_GENSCRIPTS
 /**
@@ -74,12 +143,146 @@ private:
 };
 #endif // !SCR_GENSCRIPTS
 
+
+
+#if !SCR_GENSCRIPTS
+/**
+ * Utility Class: ChessBoard
+ *
+ * Stores and analyzes the instantaneous state of a chess game.
+ */
+class ChessBoard
+{
+public:
+
+// squares
+
+	enum File
+	{
+		FILE_NONE = 0,
+		FILE_a = 'a',
+		FILE_b = 'b',
+		FILE_c = 'c',
+		FILE_d = 'd',
+		FILE_e = 'e',
+		FILE_f = 'f',
+		FILE_g = 'g',
+		FILE_h = 'h'
+	};
+
+	enum Rank
+	{
+		RANK_NONE = 0,
+		RANK_1 = '1',
+		RANK_2 = '2',
+		RANK_3 = '3',
+		RANK_4 = '4',
+		RANK_5 = '5',
+		RANK_6 = '6',
+		RANK_7 = '7',
+		RANK_8 = '8'
+	};
+
+	struct Square
+	{
+		Square (File _file = FILE_NONE, Rank _rank = RANK_NONE);
+		operator bool () const;
+		Square Offset (Square by) const;
+
+		File file;
+		Rank rank;
+	};
+
+// pieces
+
+	enum Camp
+	{
+		CAMP_NONE = 0,
+		CAMP_WHITE,
+		CAMP_BLACK
+	};
+
+	enum PieceType
+	{
+		PIECE_NONE = ' ',
+		PIECE_KING = 'k',
+		PIECE_QUEEN = 'q',
+		PIECE_ROOK = 'r',
+		PIECE_BISHOP = 'b',
+		PIECE_KNIGHT = 'n',
+		PIECE_PAWN = 'p'
+	};
+
+	struct Piece
+	{
+		Piece (Camp _camp = CAMP_NONE, PieceType _type = PIECE_NONE);
+		Piece (char code);
+		operator char () const;
+		Camp camp;
+		PieceType type;
+	};
+
+// state information
+
+	enum Castling
+	{
+		CASTLE_NONE = 0,
+		CASTLE_KINGSIDE = 1,
+		CASTLE_QUEENSIDE = 2
+	};
+
+	char piece_placement[65];
+	static const char INITIAL_PLACEMENT[65];
+
+	Camp active_camp;
+	int castling_white;
+	int castling_black;
+	Square en_passant_square;
+	uint halfmove_clock;
+	uint fullmove_number;
+
+// constructor and data access
+
+	ChessBoard ();
+
+	Piece operator[] (const Square& square) const;
+
+// representations and persistence
+
+#define FEN_BUFSIZE 128
+	void EncodeFEN (char* buffer);
+
+	void Load (object store);
+	void Save (object store);
+
+// possible moves analysis
+
+	struct Move
+	{
+		Square from;
+		Square to;
+	};
+	typedef std::vector<Move> MoveSet;
+
+	void EnumerateMoves (MoveSet& set);
+
+private:
+	void TryMovePath (MoveSet& set, Piece piece, Square origin,
+		char file_dirn, char rank_dirn);
+	bool TryMovePoint (MoveSet& set, Piece piece, Square origin,
+		char file_vect, char rank_vect,
+		bool if_hostile = true, bool if_empty = true);
+};
+#endif // !SCR_GENSCRIPTS
+
+
+
 /**
  * Script: ChessGame
  * Inherits: BaseScript
  *
- * Coordinates the chess gameplay with the GNU Chess engine, managing the Dark
- * Engine board representation (AI chess piece states and positions).
+ * Coordinates the chess gameplay, managing the board state, player controls,
+ * movement/capture/promotion effects, and ChessEngine connection.
  */
 #if !SCR_GENSCRIPTS
 class cScr_ChessGame : public virtual cBaseScript
@@ -91,14 +294,75 @@ protected:
 	virtual long OnBeginScript (sScrMsg* pMsg, cMultiParm& mpReply);
 	virtual long OnEndScript (sScrMsg* pMsg, cMultiParm& mpReply);
 	virtual long OnSim (sSimMsg* pMsg, cMultiParm& mpReply);
+	virtual long OnMessage (sScrMsg* pMsg, cMultiParm& mpReply);
 
 private:
+	object GetSquare (ChessBoard::Square square);
+	object GetPieceAt (ChessBoard::Square square);
+
+	void AnalyzeBoard ();
+
+	void SelectPiece (object piece);
+	void DeselectPieces ();
+
+	void MakeMove (object square);
+
 	ChessEngine* engine;
-	script_str position;
+	ChessBoard board;
 };
 #else // SCR_GENSCRIPTS
 GEN_FACTORY("ChessGame","BaseScript",cScr_ChessGame)
 #endif // SCR_GENSCRIPTS
+
+
+
+/**
+ * Script: ChessSquare
+ * Inherits: BaseScript
+ *
+ * Handles player interaction with a single square on the chess board.
+ */
+#if !SCR_GENSCRIPTS
+class cScr_ChessSquare : public virtual cBaseScript
+{
+public:
+	cScr_ChessSquare (const char* pszName, int iHostObjId);
+
+protected:
+	virtual long OnMessage (sScrMsg* pMsg, cMultiParm& mpReply);
+	virtual long OnFrobWorldEnd (sFrobMsg* pMsg, cMultiParm& mpReply);
+};
+#else // SCR_GENSCRIPTS
+GEN_FACTORY("ChessSquare","BaseScript",cScr_ChessSquare)
+#endif // SCR_GENSCRIPTS
+
+
+
+/**
+ * Script: ChessPiece
+ * Inherits: BaseAIScript
+ *
+ * Coordinates the activity of an AI chess piece with the ChessGame ("TheGame").
+ */
+#if !SCR_GENSCRIPTS
+class cScr_ChessPiece : public virtual cBaseAIScript
+{
+public:
+	cScr_ChessPiece (const char* pszName, int iHostObjId);
+
+protected:
+	virtual long OnMessage (sScrMsg* pMsg, cMultiParm& mpReply);
+	virtual long OnFrobWorldEnd (sFrobMsg* pMsg, cMultiParm& mpReply);
+
+private:
+	void Select ();
+	void Deselect ();
+};
+#else // SCR_GENSCRIPTS
+GEN_FACTORY("ChessPiece","BaseAIScript",cScr_ChessPiece)
+#endif // SCR_GENSCRIPTS
+
+
 
 #endif // CUSTOM_H
 
